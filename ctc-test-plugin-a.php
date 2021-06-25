af3039c4702bf1096e398b0daa43f3eefc7d9d09
ConanTheCoder <53530622+ConanTheCoder@users.noreply.github.com>
2021-06-25 22:46:34 +0200

    Add files via upload

---
+++ ctc-test-plugin-a.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name: Test-Plugin A
+ * Description: A simple plugin to test the functionality of the PluginManager
+ */
+ <?php
+add_shortcode('test_plugin_a', 'test_plugin_a_dothing');
+function test_plugin_a_init() {
+  function test_plugin_a_dothing() {
+    $output = 'Ich bin das Test-Plugin A und ich funktioniere!';
+    return $output;
+  }
+}
+add_action('init', 'test_plugin_a_init');
+?>
